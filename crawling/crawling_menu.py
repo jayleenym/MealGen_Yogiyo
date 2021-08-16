fb156b866f39599dbaaeb7f80f68cadf22622456
Yejin (Jayleen) Moon <jayleenym@gmail.com>
2021-08-16 12:16:06 +0900

    Update crawling_menu.py

--- crawling/crawling_menu.py
+++ crawling/crawling_menu.py
@@ -1,6 +1,6 @@
 from update_db import *
 
-server = UpdateMealgen(file = "connection.txt")
+server = UpdateMealgen(file = "../connection.txt")
 server.controller._connection_info()
 
 # daily crawling
