5661dc6bf03d3e48e7e1266a4d183167a6a969ec
jayleenym <jayleenym@gmail.com>
2021-09-17 10:19:41 +0900

    Updated mapping.py #2

--- mapping.py
+++ mapping.py
@@ -8,41 +8,28 @@ import json
 from tqdm import tqdm
 
 
-# 서울 데이터 업데이트
 with open(os.path.join(sys.path[0], './connection.txt'), "r") as f:
                 connect_info = list(map(lambda x: x.strip(), f.read().split(",")))
 server = MysqlController(*connect_info)
 
+# 서울 메뉴 데이터 가져오기
 server.curs.execute("SELECT DISTINCT restaurant_id FROM restaurant_info WHERE sido = '서울특별시'")
 res_id = [i[0] for i in server.curs.fetchall()]
-# res_id[:10]
 
-# 표준화된 파일
-# path = '/Users/yejin/Downloads/NAVERWORKS/서울지역 메뉴 필터링_최종본_ver.0.2_210819 (1).xlsx'
-# STD_MN = pd.read_excel(path, sheet_name=2, header = 0)
-
-# STD_MN.drop(columns = '담당자', inplace = True)
-# STD_MN.drop(STD_MN[STD_MN['표준 메뉴(MT)'] == '삭제'].index.values, inplace = True)
-# STD_MN.drop(STD_MN[STD_MN['표준 메뉴(MT)'] == '하단정렬'].index.values, inplace = True)
-# STD_MN = STD_MN[['메뉴명', '표준 메뉴(MN)', 'MN CODE']]
-
-# STD_MN.reset_index(drop = True, inplace = True)
-# MN_CODE = pd.read_excel(path, sheet_name=3, header=0)
+# 표준화 테이블
 MN_CODE = pd.read_sql("SELECT STD_MENU_NAME, STD_MENU_CODE FROM STD_MENU;", server.conn)
 
 for id in tqdm(res_id):
     df = pd.read_sql(f"SELECT menu_id, restaurant_id, name, name as name_2, price FROM menu_info WHERE restaurant_id = {id}", server.conn)
-    # df = df.append(df2)
     # 메뉴에서 띄어쓰기 지우기
     df.name_2 = df.name_2.str.replace(" ", "")
-    # df.tail()
+    
     df['std_mn'] = pd.Series(np.zeros(len(df)))
     df['std_mn_code'] = pd.Series(np.zeros(len(df)))
-    # df.head()
-
+    
     # 1차: MN_CODE에 있는거 먼저
-    for mn in range(len(MN_CODE[:-2])):
-        # 완전 일치 먼저
+    for mn in range(len(MN_CODE[:-1])):
+        # 완전 일치
         idx_p = list(df[df.name_2 == MN_CODE.loc[mn, 'STD_MENU_NAME']].index)
         for p in idx_p:
             df.loc[p, 'std_mn'] = MN_CODE.loc[mn, 'STD_MENU_NAME']
@@ -59,16 +46,7 @@ for id in tqdm(res_id):
 
     df.drop(columns = 'name_2', inplace = True)
 
-    # JSON = df.to_json(orient='records', force_ascii=False)
-    # JSON = json.loads(JSON)
-    # # JSON
-
-    # for j in JSON:
-    #     j['updated_at'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    #     try:
-    #         Insert(server, table_name = "seoul_menu", line = j) # 0 값으로 되어 있는거 빼고 업로드
-    #     except:
-    #         continue
+    # 한꺼번에 올리기
     temp=[]
     df = df.values
     temp = list(map(tuple,df))
@@ -81,4 +59,4 @@ for id in tqdm(res_id):
         server.curs.executemany(q,temp)
         server.conn.commit()
     except:
-        continue
+        continue # 이미 있거나 오류
