386998c0b5b6c598fefc4b96c34a2a06b31af644
jayleenym <jayleenym@gmail.com>
2021-09-22 23:32:33 +0900

    Updated update_user.py #1

--- curation/update_user.py
+++ curation/update_user.py
@@ -10,7 +10,6 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 # db management libraries
 import pymysql
-from dbconfig import Insert, Upsert
 from controller import MysqlController
 
 
