67dab246149c9c0de7c628ed493cb8e763a49d13
jayleenym <jayleenym@gmail.com>
2021-10-15 22:29:13 +0900

    reviews 테이블 업데이트 #3

--- curation/update_user.py
+++ curation/update_user.py
@@ -26,6 +26,7 @@ class UpdateUser():
                 connect_info = list(map(lambda x: x.strip(), f.read().split(",")))
         self.controller = MysqlController(*connect_info)
     
+    
     def update_info(self):
         q = """
         SELECT DISTINCT(CONCAT(res.sigungu, "", rev.nickname)) as user_name,
@@ -69,7 +70,51 @@ class UpdateUser():
             """
             self.controller.curs.execute(q)
             self.controller.conn.commit()
-            # Upsert(server, table_name='reviews', line=dict(df.iloc[i]), update='user_name')
+
+    def review_username(self):
+        # reviews에 user_name 업데이트하기 (누락 업데이트용)
+        reviews = pd.read_sql("SELECT review_id, nickname, restaurant_id FROM reviews;", self.controller.conn)
+        restaurant = pd.read_sql("SELECT restaurant_id, sido, sigungu FROM restaurant_info", self.controller.conn)
+        user = pd.read_sql('SELECT user_name, user_id FROM user_info', self.controller.conn)
+
+        df = reviews.merge(restaurant, on = 'restaurant_id')
+        df['user_name'] = df.sido.apply(lambda x: x[:2]) + df.sigungu + df.nickname
+        df = df.merge(user, on = 'user_name')
+
+        tp = list(map(tuple, df[['user_name', 'user_id', 'review_id']].values))
+
+        for t in tqdm(tp):
+            self.controller.curs.execute(f'''UPDATE reviews SET user_name ='{t[0]}', user_id = {t[1]} WHERE review_id = {t[2]};''')
+            self.controller.conn.commit()
+
+        self.controller.conn.close()
+
+    def review_userid(self):
+        # reviews에 userid 업데이트하기 (누락 업데이트용)
+        q = """SELECT rev.nickname, res.sido, res.sigungu, rev.review_id
+            FROM reviews as rev
+            JOIN restaurant_info as res
+            ON rev.restaurant_id = res.restaurant_id;"""
+        df = pd.read_sql(q, self.controller.conn)
+
+        rev = pd.read_sql("SELECT user_name, user_id FROM user_info;", self.controller.conn)
+
+        df['user_name'] = df.sido.apply(lambda x: x[:2]) + df.sigungu + df.nickname
+        ds = df.merge(rev, on = 'user_name')
+
+        tmp = list(map(tuple, ds[['user_name', 'user_id', 'review_id']].values))
+        print('start Updating...')
+        for t in tmp:
+            update_q = f"""UPDATE reviews
+                    SET user_name = '{t[0]}', user_id = {t[1]},
+                    updated_at = '{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+                    WHERE user_name = {t[2]};"""
+            self.controller.curs.execute(update_q)
+            self.controller.conn.commit()
+
+        print('finished')
+        self.controller.conn.close()
+
 
 if __name__ == "__main__":
     user = UpdateUser(file = "../connection.txt")
